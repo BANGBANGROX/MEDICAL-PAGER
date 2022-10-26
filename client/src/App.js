b453d89cc1f4821d21e179c7c4043191e3b0060c
BANGBANGROX <lakshya0809bang@gmail.com>
2022-10-26 10:59:52 +0530

    Added SignUp from frontend

--- client/src/App.js
+++ client/src/App.js
@@ -5,13 +5,23 @@ import { ChannelListContainer, ChannelContainer, Auth } from "./components";
 import "./App.css";
 
 const client = StreamChat.getInstance(process.env.REACT_APP_STREAM_API_KEY);
+const cookies = new Cookies();
+const authToken = cookies.get("token");
 
-const authToken = false;
+if (authToken) {
+    client.connectUser({
+        token: cookies.get("token"),
+        name: cookies.get("username"),
+        fullName: cookies.get("fullName"),
+        id: cookies.get("userId"),
+        phoneNumber: cookies.get("phoneNumber"),
+        image: cookies.get("avatarURL"),
+        hashedPassword: cookies.get("hashedPassword"),
+    }, authToken);
+}
 
 const App = () => {
-    if (!authToken) {
-        return <Auth />
-    }
+    if (!authToken) return <Auth />;
 
     return (
         <div className="app__wrapper">
--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -3,6 +3,8 @@ import Cookies from "universal-cookie";
 import axios from "axios";
 import SigninImage from "../assets/signup.jpg"
 
+const cookies = new Cookies();
+
 const initialState = {
     fullName: "",
     username: "",
@@ -24,10 +26,24 @@ const Auth = () => {
         setIsSignup((prevIsSignup) => !prevIsSignup);
     }
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
 
-        console.log(form);
+        const { fullName, username, password, phoneNumber, avatarURL } = form;
+        const URL = "http://localhost:5000/auth";
+        const { data: { token, userId, hashedPassword } } = await axios.post(`${URL}/${isSignup ? "signup" : "login"}`, { username, password, fullName, phoneNumber, avatarURL });
+        cookies.set("token", token);
+        cookies.set("username", username);
+        cookies.set("fullName", fullName);
+        cookies.set("userId", userId);
+
+        if (isSignup) {
+            cookies.set("phoneNumber", phoneNumber);
+            cookies.set("avatarURL", avatarURL);
+            cookies.set("hashedPassword", hashedPassword);
+        }
+
+        window.location.reload();
     }
 
     return (
